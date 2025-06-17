5555aa5e85651a32ae4059658ab08d40159ace16
scootdboot <andersonscott100@gmail.com>
2025-06-17 13:05:27 -0600

    change App for different testing

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println(Color.fromText("⬛️").isPresent());
+        System.out.println(Color.fromText("⬛️").get().textRepresentation);
+
         WordList wordList = new WordList("/Words.txt");
         System.out.println(wordList.getWords().get().get(0));
         System.out.println(wordList.getWords().get().get(4999));
