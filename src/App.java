7aa778c611eb850e3e9b9fed86b6ecc40c92d0fd
scootdboot <andersonscott100@gmail.com>
2025-06-17 12:44:35 -0600

    remove filepath from App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // WordList wordList = new WordList("/Users/scootdboot/Desktop/code/wordle-shape-finder/src/Words.txt");
         WordList wordList = new WordList("/Words.txt");
         System.out.println(wordList.getWords().get().get(0));
         System.out.println(wordList.getWords().get().get(4999));
