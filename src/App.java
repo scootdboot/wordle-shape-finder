80d8b64c0c996d66568aad901ad8f04ed05989c0
scootdboot <andersonscott100@gmail.com>
2025-06-17 13:06:30 -0600

    fix word file location, we're so back

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ public class App {
         System.out.println(Color.fromText("⬛️").isPresent());
         System.out.println(Color.fromText("⬛️").get().textRepresentation);
 
-        WordList wordList = new WordList("/Words.txt");
+        WordList wordList = new WordList("Words.txt");
         System.out.println(wordList.getWords().get().get(0));
         System.out.println(wordList.getWords().get().get(4999));
         System.out.println(wordList.getWords().get().get(14853));
