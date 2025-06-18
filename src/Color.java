f4e838ed59f3c944fb3f41df6ae94b5b0d0da056
scootdboot <andersonscott100@gmail.com>
2025-06-18 01:17:12 -0600

    add emoji output to Color

--- src/Color.java
+++ src/Color.java
@@ -1,21 +1,23 @@
 import java.util.Optional;
 
 public enum Color {
-    GRAY('_'),
-    YELLOW('Y'),
-    GREEN('G');
+    GRAY('_', "⬛️"),
+    YELLOW('Y', "🟨"),
+    GREEN('G', "🟩");
 
-    public final char charRepresentation;
+    public final char asciiRepresentation;
+    public final String emojiRepresentation;
 
-    private Color(char c) {
-        charRepresentation = c;
+    private Color(char c, String emoji) {
+        asciiRepresentation = c;
+        emojiRepresentation = emoji;
     }
 
     // even though this takes a String, it should really just
     // be an emoji (1 char in human terms)! don't be fooled!
     public static Optional<Color> fromCharacter(char c) {
         for (Color color : Color.values()) {
-            if (c == color.charRepresentation) {
+            if (c == color.asciiRepresentation) {
                 return Optional.of(color);
             }
         }
