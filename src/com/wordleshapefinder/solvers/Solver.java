55f28cc5f2e60af022a09f803a63a7c294ed2a22
scootdboot <andersonscott100@gmail.com>
2025-06-19 13:03:56 -0600

    slight update to package names

--- src/com/wordleshapefinder/solvers/Solver.java
+++ src/com/wordleshapefinder/solvers/Solver.java
@@ -1,10 +1,14 @@
-package com.wordleshapefinder.solver;
+package com.wordleshapefinder.solvers;
 
 import java.util.Optional;
 
 import com.wordleshapefinder.WordList;
 import com.wordleshapefinder.shapeutils.Row;
 
+/**
+ * This is an interface for Solvers to ensure that creating a Solver that functions differently
+ *  and implementing it is minimally complicated.
+ */
 public interface Solver {
     public void intakeWordList(WordList wordList);
 
